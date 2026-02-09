b7f7b850046fb617d69433d069bc7b9eb0fcd860
Black21396 <fadi.199620@gmail.com>
2026-02-09 16:46:43 +0100

    Add builder annotation to EmployeeDto

--- src/main/java/net/fadi/ems/dto/EmployeeDto.java
+++ src/main/java/net/fadi/ems/dto/EmployeeDto.java
@@ -2,9 +2,15 @@ package net.fadi.ems.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonPropertyOrder({ "id", "firstName", "lastName", "email" })
 public class EmployeeDto {
     private Long id;
